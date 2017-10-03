ac98e9bc2fa4b9346400a75109fbcb26ce141995
Neal <nealvis@gmail.com>
2017-10-02 23:07:24 -0700

    download model script

---
+++ scripts/download_model.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PROTOTXT_FILENAME= deploy.prototxt
+#GET_PROTOTXT = wget -P . https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/deploy.prototxt
+
+#CAFFEMODEL_FILENAME = bvlc_googlenet.caffemodel
+#GET_CAFFEMODEL = wget -P . -N http://dl.caffe.berkeleyvision.org/${CAFFEMODEL_FILENAME}
+
+wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/solver.prototxt
+sed -i 's/models\/bvlc_googlenet\/train_val.prototxt/elvisnet_googlenet\/train_val.prototxt/' solver.prototxt
+sed -i 's/models\/bvlc_googlenet\/bvlc_googlenet/elvisnet_googlenet\/caffe_googlenet_train/' solver.prototxt
+
+wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/train_val.prototxt
+
+mkdir elvisnet_googlenet
+mv solver.prototxt elvisnet_googlenet/
+mv train_val.prototxt elvisnet_googlenet/
+
