db1a93d4a2d0a3f07c01cc7cd9738f40675093af
Neal <nealvis@gmail.com>
2017-10-28 14:35:19 -0700

    just checking in the prototxt files instead of downloading and modifying

--- scripts/convert_lmdb.sh
+++ scripts/convert_lmdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GLOG_logtostderr=1 convert_imageset --resize_height=256 --resize_width=256 --shuffle ./ ./train_index.txt ./ilsvrc12_train_lmdb
+GLOG_logtostderr=1 convert_imageset --resize_height=256 --resize_width=256 --shuffle ./ ./train_index.txt ./elvisnet_train_lmdb
 
-GLOG_logtostderr=1 convert_imageset --resize_height=256 --resize_width=256 --shuffle ./ ./test_index.txt ./ilsvrc12_test_lmdb
+GLOG_logtostderr=1 convert_imageset --resize_height=256 --resize_width=256 --shuffle ./ ./test_index.txt ./elvisnet_test_lmdb
 
 
--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -6,13 +6,14 @@
 #CAFFEMODEL_FILENAME = bvlc_googlenet.caffemodel
 #GET_CAFFEMODEL = wget -P . -N http://dl.caffe.berkeleyvision.org/${CAFFEMODEL_FILENAME}
 
-wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/solver.prototxt
-sed -i 's/models\/bvlc_googlenet\/train_val.prototxt/elvisnet_googlenet\/train_val.prototxt/' solver.prototxt
-sed -i 's/models\/bvlc_googlenet\/bvlc_googlenet/elvisnet_googlenet\/caffe_googlenet_train/' solver.prototxt
+#wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/solver.prototxt
+#sed -i 's/models\/bvlc_googlenet\/train_val.prototxt/elvisnet_googlenet\/train_val.prototxt/' solver.prototxt
+#sed -i 's/models\/bvlc_googlenet\/bvlc_googlenet/elvisnet_googlenet\/caffe_googlenet_train/' solver.prototxt
 
-wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/train_val.prototxt
-sed -i 's/^.*mean_value:.*$//g' ./train_val.prototxt
-sed -i 's/transform_param {/transform_param {\n    mean_file: "elvisnet_mean.binaryproto"/g' ./train_val.prototxt
+#wget -P . http://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_googlenet/train_val.prototxt
+#sed -i 's/^.*mean_value:.*$//g' ./train_val.prototxt
+#sed -i 's/transform_param {/transform_param {\n    mean_file: "elvisnet_mean.binaryproto"/g' ./train_val.prototxt
+
 
 mkdir elvisnet_googlenet
 mv solver.prototxt elvisnet_googlenet/
